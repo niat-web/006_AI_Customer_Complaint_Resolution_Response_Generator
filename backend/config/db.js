a77f3e8e352fddb405c110dc12c6229aec94270a
Priyanshu Raj <priyanshuraj9595@gmail.com>
2026-06-11 11:53:23 +0530

    update in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,21 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+let db;
+
+if (process.env.DATABASE_URL) {
+  db = mysql.createPool(process.env.DATABASE_URL);
+} else {
+  db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+}
 
 module.exports = db;
